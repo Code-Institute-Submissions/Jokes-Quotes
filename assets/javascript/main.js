59a43ba307da8fe45b0e45cd35a70047230d50e5
JohnW876 <john@illumino.co.uk>
2020-09-26 16:15:05 +0000

    Used Javascript to create joke selector dropdown. Tested to ensure data being returned from api and rendering to user

---
+++ assets/javascript/main.js
@@ -0,0 +1,36 @@
+var typeSelector = ["programming", "general", "knock-knock"];
+var joke_result = document.getElementById("joke_result");
+var joke_btn = document.getElementById("joke_btn");
+
+joke_btn.addEventListener('click', selectJoke);
+function selectJoke() {
+    const userChoice = document.getElementById("joke-choice").value;
+    getRandomJoke(userChoice);
+}
+function renderToUser(joke) {
+    document.getElementById("joke_result").innerHTML = `<p>${joke.setup}</p>
+    <p>${joke.punchline}</p>`
+}
+async function getRandomJoke(category) {
+    fetch(`https://official-joke-api.appspot.com/jokes/${category}/random`)
+        .then(res => res.json())
+        .then(data => {
+            renderToUser(data[0])
+        }).catch(e => {
+        })
+}
+function selectedData(element) {
+    const userChoice = element.value;
+    getRandomJoke(userChoice);
+}
+function createTypeList(data) {
+    document.getElementById("type").innerHTML = `
+        <select id="joke-choice" class="custom-select custom-select-sm">
+                <option>Select a joke type</option>
+                ${typeSelector.map(function (type) {
+                    return `<option>${type}</option>`
+                })}
+            </select>
+    `
+}
+createTypeList();
